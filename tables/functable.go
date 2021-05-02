425505984c309b4b5abdba843ee283b896e9daa2
isabelcruz816 <isabel.cruz81699@gmail.com>
2021-05-02 12:50:03 -0500

    Added Functable

--- tables/functable.go
+++ tables/functable.go
@@ -1,97 +1,60 @@
 package tables
 
-// import(
-// 	"github.com/uliseslugo/ilusa_pp/types"
-// 	"github.com/uliseslugo/ilusa_pp/gocc/token"
-// 	"fmt"
-// )
+import (
+	"github.com/uliseslugo/ilusa_pp/types"
+)
 
-// // function row struct
-// type FuncRow struct {
-// 	id string
-// 	return_val types.CoreType
-// 	params []types.CoreType
-// 	scope int
-// 	local_vars *VarTable // ask elda
-// }
+// function row struct
+type FuncRow struct {
+	id         string
+	return_val types.CoreType
+	params     []types.CoreType
+	// scope int
+	// local_vars *VarTable // ask elda
+}
 
-// // fuction table struct
-// type FuncTable struct {
-// 	table map[string]*FuncRow
-// }
+// fuction table struct
+type FuncTable struct {
+	table map[string]*FuncRow
+}
 
-// // Getter for id
-// func (fr *FuncRow) Id() string {
-// 	return fr.id
-// }
+// Getter for id
+func (fr *FuncRow) Id() string {
+	return fr.id
+}
 
-// // Setter for id
-// func (fr *FuncRow) SetId(curr_id string) {
-// 	fr.id = curr_id
-// }
+// Setter for id
+func (fr *FuncRow) SetId(curr_id string) {
+	fr.id = curr_id
+}
 
-// // Getter for return value
-// func (fr *FuncRow) ReturnValue() string {
-// 	return fr.return_val
-// }
+// Getter for return value
+func (fr *FuncRow) ReturnValue() types.CoreType {
+	return fr.return_val
+}
 
-// // Setter for return value
-// func (fr *FuncRow) ReturnValue(curr_type types.CoreType) string {
-// 	fr.return_val = curr_type
-// }
+// Setter for return value
+func (fr *FuncRow) SetReturnValue(curr_type types.CoreType) {
+	fr.return_val = curr_type
+}
 
-// // Getter for params
-// func (fr *FuncRow) Params() []types.CoreType {
-// 	return fr.params
-// }
+// Getter for params
+func (fr *FuncRow) Params() []types.CoreType {
+	return fr.params
+}
 
-// // Setter for params
-// func (fr *FuncRow) SetParams(curr_params []types.CoreType) {
-// 	fr.params = curr_params
-// }
+// Setter for params
+func (fr *FuncRow) SetParams(curr_params []types.CoreType) {
+	fr.params = curr_params
+}
 
-// // Getter for scope
-// func (fr *FuncRow) Scope() {
-// 	return fr.scope
-// }
+// Getter for Function Table
+func (ft *FuncTable) Table() map[string]*FuncRow {
+	return ft.table
+}
 
-// // Setter for scope
-// func (fr *FuncRow) SetScope(curr_scope int) {
-// 	fr.scope = curr_scope
-// }
-
-// // Getter for local variables entry
-// func (fr *FuncRow) LocalVars() {
-// 	return fr.local_vars
-// }
-
-// // Setter for local variables entry
-// func (fr *FuncRow) LocalVars(curr_vars *VarTable) {
-// 	fr.local_vars = curr_vars
-// }
-
-// // Getter for Function Table
-// func (fr *FuncRow) Table() map[string]*FuncRow {
-// 	return fr.table
-// }
-
-// // Add Function Row to Table
-// func (ft *FuncTable) AddRow(row *FuncRow) bool {
-// 	// // check if key is inserted in map
-// 	// val, ok := ft.table[e.Id()]
-
-// 	// if !ok {
-// 	// 	// add key
-// 	// 	ft.table[row.Id()] = row
-// 	// }
-
-// 	// return !ok
-
-// 	row.SetId("newFunction")
-// 	fmt.Println(row.Id())
-// 	return true
-
-// }
-
-// TODO(isabel) Add tests for rest of vartable.go
-// Signal tested function with flag at the beginning
+// Add Function Row to Table
+func (ft *FuncTable) AddRow(row *FuncRow) bool {
+	row.SetId("newFunction")
+	return true
+}
