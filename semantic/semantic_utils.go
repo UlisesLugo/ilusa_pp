650526feca85559acfaf2d09ab6b77d44adc0e94
Ulises Lugo <uliseslf99@hotmail.com>
2021-05-06 16:07:19 -0500

    Semantic map ready

--- semantic/semantic_utils.go
+++ semantic/semantic_utils.go
@@ -2,14 +2,24 @@ package semantic
 
 import (
 	"errors"
+	"strconv"
 
 	"github.com/uliseslugo/ilusa_pp/types"
 )
 
 func GetReturnType(op Operation, type1, type2 types.CoreType) (types.CoreType, error) {
-	result := SemanticCube[op][type1][type2]
-	if result == types.Null {
-		return result, errors.New("Unsupported operation")
+	semantic_cube := NewSemanticCube()
+
+	if int(type1) > int(type2) {
+		temp := type1
+		type1 = type2
+		type2 = temp
+	}
+
+	cube_key := string(op) + strconv.Itoa(int(type1)) + strconv.Itoa(int(type2))
+	result, ok := semantic_cube.operations[cube_key]
+	if !ok {
+		return result, errors.New("unsupported operation")
 	}
 	return result, nil
 }
