d727ed3eea597919e923c3e6a86684f8aae774d5
isabelcruz816 <isabel.cruz81699@gmail.com>
2021-05-24 21:50:11 -0500

    run time memory

--- virtual_machine/virtual_machine.go
+++ virtual_machine/virtual_machine.go
@@ -1,1 +1,19 @@
 package virtual_machine
+
+import (
+	"github.com/uliseslugo/ilusa_pp/memory"
+	"github.com/uliseslugo/ilusa_pp/quadruples"
+	"github.com/uliseslugo/ilusa_pp/tables"
+)
+
+type VirtualMachine struct {
+	funcTable tables.FuncTable
+	quads     []quadruples.Cuadruplo
+	memory    memory.VirtualMemory
+	ip        int // instruction pointer
+	// TODO: Add functions attributes (Act_Records)
+}
+
+// TODO: setValue
+// SetValue takes a value and an address and finds the corresponding position to that address and
+// tries to save the given value
