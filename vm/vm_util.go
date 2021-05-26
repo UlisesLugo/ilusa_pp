a43f7a8775d4f35e12737257801ce7f44da97543
isabelcruz816 <isabel.cruz81699@gmail.com>
2021-05-26 16:31:34 -0500

    AST working with linear expressions

---
+++ vm/vm_util.go
@@ -0,0 +1,28 @@
+package vm
+
+import "os"
+
+// readFile
+func readFile(path string) ([]byte, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+
+	defer file.Close()
+
+	fileinfo, err := file.Stat()
+	if err != nil {
+		return nil, err
+	}
+
+	filesize := fileinfo.Size()
+	buffer := make([]byte, filesize)
+
+	_, err = file.Read(buffer)
+	if err != nil {
+		return nil, err
+	}
+
+	return buffer, nil
+}
